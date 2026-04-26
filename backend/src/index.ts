8cf7619ef92c63fc6ed2aad7345e4c5863fbb6f2
Faris Mohammad <farisghrouph@gmail.com>
2026-04-26 19:54:43 +0300

    initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors'
+import {ENV} from './config/env.ts';
+import { clerkMiddleware } from '@clerk/express'
+
+const app = express();
+
+app.use(cors({origin: ENV.FRONTEND_URL}))
+app.use(clerkMiddleware()); // auth object will be attached to the req object
+app.use(express.json()); // parses json request bodies
+app.use(express.urlencoded({extended: true})); // parses form data (like html forms)
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Welcome to Productify API - Powered by PostgreSQL, Drizzle ORM & Clerk Auth',
+        endpoints: {
+            users: '/api/users',
+            products: '/api/products',
+            comments: '/api/comments'
+        }
+    })
+})
+
+app.listen(ENV.PORT, () => console.log('Server is up and running on port 3000'));
